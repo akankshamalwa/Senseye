ba0d985b40488d29fccee41702d3c5338fcf553d
akankshamalwa <malwadeakanksha@gmail.com>
2023-09-28 15:06:59 +0530

    Added code comment and made some cleanup

--- Aumni_Assignment/ContentView.swift
+++ Aumni_Assignment/ContentView.swift
@@ -8,45 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // MARK: Properties
+
+    // State property to store the text entered by the user.
     @State private var text: String = ""
+
+    // State property to track whether the "Click Me" button is tapped.
     @State private var isClickMeButtonTapped: Bool = false
+
+    // Observed object for the view model.
     @ObservedObject var viewModel: NicknameViewModel = NicknameViewModel()
 
     var body: some View {
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.white).opacity(0.9)
-                    .frame(width: 380, height: 300)
-                    .shadow(color: .gray, radius: 5)
-                VStack(alignment: .center, spacing: 20) {
-                    Text("Username")
-                        .font(.title3)
-                    TextField("Enter Username", text: $text)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
-                    Button {
-                        isClickMeButtonTapped = true
-                        viewModel.clickMeTapped(user: text)
-                    } label: {
-                        RoundedRectangle(cornerRadius: 15)
-                            .frame(width: 320, height: 50)
-                            .foregroundColor(.cyan)
-                            .overlay(Text("Click Me!!")
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .font(.title2))
-                    }
-
-                    if isClickMeButtonTapped {
-                        Text("Here is your nickname:\(viewModel.name)")
-                            .foregroundColor(.blue)
-                            .font(.headline)
-                            .fontWeight(.bold)
-                    }
-
-                }
+        ZStack {
+            containerView
+            cellView
         }
         .padding()
+    }
+
+    var cellView: some View {
+        VStack(alignment: .center, spacing: 20) {
+            Text("Username")
+                .font(.title3)
+
+            // Text field for entering the username.
+            TextField("Enter Username", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            // "Click Me" button.
+            Button {
+                isClickMeButtonTapped = true
+                viewModel.clickMeTapped(user: text)
+            } label: {
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 320, height: 50)
+                    .foregroundColor(.cyan)
+                    .overlay(Text("Click Me!!")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .font(.title2))
+            }
+
+            if isClickMeButtonTapped {
+
+                // Display the user's nickname if the button is tapped.
+                Text("Here is your nickname:\(viewModel.name)")
+                    .foregroundColor(.blue)
+                    .font(.headline)
+                    .fontWeight(.bold)
+            }
+
+        }
+    }
+
+    var containerView: some View {
+
+        // Container view with rounded corners and shadow.
+        RoundedRectangle(cornerRadius: 20)
+            .foregroundColor(.white).opacity(0.9)
+            .frame(width: 380, height: 300)
+            .shadow(color: .gray, radius: 5)
     }
 }
 
--- Aumni_Assignment/ViewModel/NicknameViewModel.swift
+++ Aumni_Assignment/ViewModel/NicknameViewModel.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 class NicknameViewModel: ObservableObject {
+
+    //MARK: Published properties
+
+    // A dictionary to store the mapping of user names to nicknames.
     @Published var mappingName: [String:String] = [:]
-    @State private var username: String?
-    private var nickNames: [String] = ["Romeo", "James Bond", "Ironman", "Thor", "Hulk", "Captain America"]
+
+    // A private property to store the current nickname.
     @Published private var nickName: String?
+
+    // A private property to store the current username.
+    @State private var username: String?
+
+    // An array of predefined nicknames.
+    private var nickNames: [String] = ["Romeo", "James Bond", "Ironman", "Thor", "Hulk", "Captain America"]
+
+    // A flag to determine if the nickname should be capitalized.
     private var shouldCapitalizedName: Bool = false
+
+    // A computed property to get the current name.
     var name: String {
         get {
             guard let name = nickName else {
@@ -22,14 +36,25 @@ class NicknameViewModel: ObservableObject {
         }
     }
 
+    // MARK: - Initializer
+
     init() {
     }
 
+    // MARK: - Methods
+
+    // Function to handle the "Click Me" button tap.
     func clickMeTapped(user: String) {
         if let existingNickname = mappingName[user] {
+
+            // Use the existing nickname for the user.
             nickName = shouldCapitalizedName ? existingNickname.uppercased() : existingNickname
         } else {
+
+            // Generate a random nickname for the user.
             nickName = shouldCapitalizedName ? nickNames.randomElement()?.uppercased() : nickNames.randomElement()
+
+            // Store the mapping of the user to the generated nickname.
             mappingName[user] = nickName
         }
     }
